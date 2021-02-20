ddcd11fc3ec003343d9c51e522059869975737fd
enaburekhan <ericenaburekhan2018@gmail.com>
2021-02-20 13:25:27 +0100

    create test for getScores and stars

---
+++ src/tests/stars.test.js
@@ -0,0 +1,15 @@
+import Phaser from 'phaser';
+import Star from '../lib/stars';
+
+test('Star is a function', () => {
+  expect(typeof Star).toBe('function');
+});
+
+test('Star is a subclass of sprite', () => {
+  expect(Star.prototype instanceof Phaser.Physics.Arcade.Sprite).toBe(true);
+});
+
+
+test('Star has a constructor', () => {
+  expect(Star.prototype.constructor).not.toBe(false);
+});
